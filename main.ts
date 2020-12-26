1c2d6515f59cc170788fea10b7200003d8c9cf24
hdenk <68135+hdenk@users.noreply.github.com>
2020-12-26 08:20:35 +0100

    Calculate and transmit accelerationSector

--- main.ts
+++ main.ts
@@ -1,9 +1,66 @@
+function accelerationSector (angle: number) {
+    if (angle < -150 || angle > 150) {
+        return 1
+    } else if (angle > -30 && angle < 30) {
+        return 2
+    } else if (angle < -60 && angle > -120) {
+        return 3
+    } else if (angle > 60 && angle < 120) {
+        return 4
+    } else {
+        return 0
+    }
+}
 function accelerationAmount (x: number, y: number) {
     return Math.trunc(Math.sqrt(x * x + y * y))
+}
+function accelerationImage (sector: number) {
+    if (sector == 1) {
+        return images.createImage(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (sector == 2) {
+        return images.createImage(`
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (sector == 3) {
+        return images.createImage(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (sector == 4) {
+        return images.createImage(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+    } else {
+        return images.createImage(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    }
 }
 function accelerationAngle (x: number, y: number) {
     return Math.round(Math.atan2(y, x) * 180 / PI)
 }
+let accSector = 0
 let accAngle = 0
 let accAmount = 0
 let accY = 0
@@ -24,51 +81,9 @@ basic.forever(function () {
     accY = input.acceleration(Dimension.Y)
     accAmount = accelerationAmount(accX, accY)
     accAngle = accelerationAngle(accX, accY)
-    basic.showString("" + (accAngle))
-    if (accX < -256) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # . . .
-            . . . . .
-            . . . . .
-            `)
-        radio.sendNumber(1)
-    } else if (accX > 256) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . # .
-            . . . . .
-            . . . . .
-            `)
-        radio.sendNumber(2)
-    } else if (accY < -256) {
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        radio.sendNumber(3)
-    } else if (accY > 256) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            `)
-        radio.sendNumber(4)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . # . .
-            . # . # .
-            . . . . .
-            `)
-        radio.sendNumber(0)
+    accSector = accelerationSector(accAngle)
+    if (accAmount > 256) {
+        accelerationImage(accSector).showImage(0)
+        radio.sendNumber(accSector)
     }
 })
