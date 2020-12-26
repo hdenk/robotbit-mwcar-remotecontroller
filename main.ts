abd2a9b3b8d24223c3eca6ee450315068a8b3345
hdenk <68135+hdenk@users.noreply.github.com>
2020-12-26 07:15:03 +0100

    Calculate accelerationAmount ad accelerationAngle

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
+function accelerationAmount (x: number, y: number) {
+    return Math.trunc(Math.sqrt(x * x + y * y))
+}
+function accelerationAngle (x: number, y: number) {
+    return Math.round(Math.atan2(y, x) * 180 / PI)
+}
+let accAngle = 0
+let accAmount = 0
 let accY = 0
 let accX = 0
+let PI = 0
 radio.setGroup(0)
 radio.setTransmitPower(7)
+PI = 3.14159265359
 basic.showLeds(`
     . . . . .
     . . # . .
@@ -12,6 +22,9 @@ basic.showLeds(`
 basic.forever(function () {
     accX = input.acceleration(Dimension.X)
     accY = input.acceleration(Dimension.Y)
+    accAmount = accelerationAmount(accX, accY)
+    accAngle = accelerationAngle(accX, accY)
+    basic.showString("" + (accAngle))
     if (accX < -256) {
         basic.showLeds(`
             . . . . .
